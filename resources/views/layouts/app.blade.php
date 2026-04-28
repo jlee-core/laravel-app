4f83d2b162bd2c794a6a5cfc3086ccdd2dc18bd6
j.lee <j.lee@core-tech.jp>
2026-04-28 15:39:17 +0900

    ログイン機能を実装

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,38 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     @vite('resources/css/style.css')
     <title>@yield('title', 'Todoアプリ')</title>
 </head>
+
 <body>
     <header>
         <h1>Todoアプリ</h1>
-
         <nav>
             <a href="{{ route('todos.index') }}">一覧</a>
             <a href="{{ route('todos.create') }}">作成</a>
         </nav>
     </header>
-
     <main>
         @yield('content')
-    </main>
+        <nav>
+            @auth
+            <a href="{{ route('todos.index') }}">投稿一覧</a>
 
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit">ログアウト</button>
+            </form>
+            @else
+            <a href="{{ route('login') }}">ログイン</a>
+            @endauth
+        </nav>
+    </main>
     <footer>
         <p>© Todo App</p>
     </footer>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\AuthController;
+use app\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 // 入り口
@@ -29,3 +31,17 @@ Route::delete('todos/{todo}', [TodoController::class, 'destroy'])
 // 検索結果ページへ遷移
 Route::get('todos/search', [TodoController::class, 'search'])
     ->name('todos.search');
+
+// ログイン機能
+Route::get('/login', [AuthController::class, 'showLogin'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login.store');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('posts', PostController::class);
+});
