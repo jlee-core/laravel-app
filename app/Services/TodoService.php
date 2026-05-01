4a3adbb887a6ffe2843f82374e31eda8c834f112
j.lee <j.lee@core-tech.jp>
2026-05-01 11:37:09 +0900

    通知機能途中

--- app/Services/TodoService.php
+++ app/Services/TodoService.php
@@ -5,16 +5,20 @@ namespace App\Services;
 use App\Models\Todo;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Auth;
+use App\Notifications\TodoCreatedNotification;
+use Illuminate\Notifications\Notifiable;
 
 class TodoService
 {
     public function create(array $data, ?UploadedFile $attachment = null): Todo
     {
         $attachmentPath = null;
+
         if ($attachment) {
             $attachmentPath = $attachment->store('todos', 'public');
         }
-        return Todo::create([
+
+        $todo = Todo::create([
             'category_id' => $data['category_id'],
             'title' => $data['title'],
             'body' => $data['body'] ?? null,
@@ -22,5 +26,9 @@ class TodoService
             'is_done' => false,
             'user_id' => Auth::id(),
         ]);
+        
+        Auth::user()?->notify(new TodoCreatedNotification());
+
+        return $todo;
     }
 }
